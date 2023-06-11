36a8b0f3e945c1713e3ae363625f94f03f452e14
Cole Gregory <cgregory902@gmail.com>
2023-06-10 22:51:07 -0500

    App skeleton working

--- app/src/main/java/com/example/rhysticbuddy/MainActivity.kt
+++ app/src/main/java/com/example/rhysticbuddy/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.rhysticbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //get reference to button
+        val buttonClick = findViewById(R.id.button) as Button
+        //set listener to button
+        buttonClick.setOnClickListener()
+        {
+            //Play sound
+            var player = MediaPlayer.create(applicationContext, R.raw.question_male)
+            player.start()
+        }
     }
 }
